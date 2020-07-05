54dcdd05f5a8f30f3d8bdecee4c071aa62b19ada
Patryk Krolikowski <krolikowskipatryk24@gmail.com>
2020-07-05 11:35:32 +0200

    Simple_Index

--- core/settings.py
+++ core/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
-    'web',
+    'users.apps.UsersConfig',
+    'web.apps.WebConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['www'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/www/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "www"),
+]
