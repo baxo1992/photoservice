29d4a6acba16b40a9664df06f67f51d99bb83595
wielkieG <pawelgrobelak@interia.pl>
2020-07-05 12:46:18 +0200

    proba dodania slidera na strone glowna

---
+++ www/script.js
@@ -0,0 +1,29 @@
+var numer = 1;
+
+var timer1 = 0;
+var timer2 = 0;
+
+function ustawslajd(nrslajdu)
+{
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    schowaj();
+    numer = nrslajdu;
+    setTimeout("zmienslajd()",500);
+}
+
+function schowaj()
+{
+    $("#slajder").fadeOut(500);
+}
+
+function zmienslajd()
+{
+    numer++; if (numer>5)numer=1;
+    var plik = "<img src=\"img/0"+numer+".jpg\"/>";
+    document.getElementById("slajder").innerHTML = plik;
+    $("#slajder").fadeIn(500);
+
+    timer1 = setTimeout("zmienslajd()",5000);
+    timer2 = setTimeout("schowaj()",4500);
+}
