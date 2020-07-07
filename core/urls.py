2f71ba830f7bd8d9cb35168a6a53194eb91482aa
Mikolaj Chrzan <mikolaj.chrzan@wmglobal.com>
2020-07-07 20:14:00 +0200

    Some views and forms added

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('user/', include('users.urls')),
+    path('user/', include('users.urls')),
     path('', include('web.urls')),
 ]\
               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
