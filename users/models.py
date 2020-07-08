a3021add9341c432b647cd2224c32ade050963fc
Patryk Krolikowski <krolikowskipatryk24@gmail.com>
2020-07-08 21:01:35 +0200

    messaging_system/ registration + signals

--- users/models.py
+++ users/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from django.contrib.auth.models import User
+from PIL import Image
 
 
 # Create your models here.
 
-class User(models.Model):
-    name = models.CharField(max_length=20)
-    surname = models.CharField(max_length=20)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=32, blank=True, null=True)
+    last_name = models.CharField(max_length=32, blank=True, null=True)
     email = models.EmailField()
 
     phone_regex = RegexValidator(regex=r'^\d{9,15}$', message='Numer telefonu powinnien posiadać od 9 do 15 cyfr!')
@@ -14,4 +17,4 @@ class User(models.Model):
     phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=True)
 
     def __str__(self):
-        return f"{self.name} {self.surname}"
+        return f'{self.user.username} Profile'
