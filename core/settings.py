b6f4550b54985270e8a10db78b57e28f22930e78
Patryk Krolikowski <krolikowskipatryk24@gmail.com>
2020-07-08 02:03:01 +0200

    login redirect

--- core/settings.py
+++ core/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/www/'
-STATIC_ROOT = 'staic'
+STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
