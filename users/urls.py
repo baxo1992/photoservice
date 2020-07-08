1e09fadc588cdd919ab07a748a9360018493e77b
Mikolaj Chrzan <mikolaj.chrzan@wmglobal.com>
2020-07-08 19:18:39 +0200

    Registration panel added

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 
-from users.views import SubmittableLoginView
+from users.views import SubmittableLoginView, register
 
 
 urlpatterns = [
     path('login/', SubmittableLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', register, name='register')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 from users.forms import (
     SubmittableAuthenticationForm,
@@ -17,3 +19,16 @@ class SubmittablePasswordChangeView(PasswordChangeView):
     form_class = SubmitablePasswordChangeForm
     template_name = 'form.html'
     success_url = reverse_lazy('index')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/users')
+    else:
+        form = UserCreationForm()
+
+        args = {'form': form}
+        return render(request, 'templates/registration/registration.html', args)
