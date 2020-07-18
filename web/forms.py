3a0ced80d1be5850d2800b6dd8f3c2a4fe4d2c80
Mateusz Kania <mateusz.kania93@gmail.com>
2020-07-18 11:57:01 +0200

    Reservation form validator

--- web/forms.py
+++ web/forms.py
@@ -2,17 +2,23 @@ from datetime import date
 
 from django import forms
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 from web.models import Reservation
 
 
-def past_date_field(value):
-    if value.date <= date.today():
-        raise ValidationError('Błędna data')
+class PastDateField(forms.DateTimeField):
+
+    def validate(self, value):
+        """Funkcja sprawdzająca czy wprowadzona data rezerwacji jest poprawna"""
+        super().validate(value)
+        if value <= timezone.now():
+            raise ValidationError('Wybrany przez ciebie termin jest nie poprawny!')
 
 
 class ReservationForm(forms.ModelForm):
-    reservation_date = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'], label='Termin sesji', help_text='(Format: dd/mm/yyyy HH:MM)')
+    reservation_date = PastDateField(input_formats=['%d/%m/%Y %H:%M'], label='Termin sesji',
+                                     help_text='(Format: dd/mm/yyyy HH:MM)')
 
     class Meta:
         model = Reservation
